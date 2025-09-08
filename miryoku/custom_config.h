43406fe42e2ec7f396a95c9b2c9f738ef9471ef1
Kevin Mayer <kevin.mayer.bb@gmail.com>
2025-09-08 15:46:18 +0200

    added battery reporting flags

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-#define CONFIG_BT_CTLR_TX_PWR_PLUS_8_y
-
-#define CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING_y
-#define CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_PROXY_y
